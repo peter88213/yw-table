1dab1b8f8fd6ca475e3ddccddcfe4483b07eacf0
peter88213 <peter.88213@t-online.de>
2022-12-20 19:59:30 +0100

    Remove local pywriter copy.

--- src/build_yw_table.py
+++ src/build_yw_table.py
@@ -8,7 +8,7 @@ License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 import os
 import sys
-# sys.path.insert(0, f'{os.getcwd()}/../../PyWriter/src')
+sys.path.insert(0, f'{os.getcwd()}/../../PyWriter/src')
 import inliner
 
 SRC = '../src/'
@@ -18,10 +18,10 @@ TARGET_FILE = f'{BUILD}yw_table.py'
 
 
 def main():
-    # inliner.run(SOURCE_FILE, TARGET_FILE, 'ywtablelib', '../src/', copyPyWriter=True)
-    # inliner.run(TARGET_FILE, TARGET_FILE, 'pywriter', '../../PyWriter/src/', copyPyWriter=True)
-    inliner.run(SOURCE_FILE, TARGET_FILE, 'ywtablelib', '../src/')
-    inliner.run(TARGET_FILE, TARGET_FILE, 'pywriter', '../src/')
+    inliner.run(SOURCE_FILE, TARGET_FILE, 'ywtablelib', '../src/', copyPyWriter=False)
+    inliner.run(TARGET_FILE, TARGET_FILE, 'pywriter', '../../PyWriter/src/', copyPyWriter=False)
+    # inliner.run(SOURCE_FILE, TARGET_FILE, 'ywtablelib', '../src/')
+    # inliner.run(TARGET_FILE, TARGET_FILE, 'pywriter', '../src/')
     print('Done.')
 
 
